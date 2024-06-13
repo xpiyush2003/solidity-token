e1814fecd518f0b8ef0499560870ef4d9af1beff
xpiyush2003 <piyushchd03@gmail.com>
2024-06-13 23:30:32 +0530

    Update Eth_beginer.sol

--- Eth_beginer.sol
+++ Eth_beginer.sol
@@ -1,24 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
-
 contract token{
     string public name = "Piyush";
     string public abbreviation = "PI";
     uint public totalsupply;
-
     mapping(address => uint) public balance;
-
     function mint(address _addr, uint _value) public {
         totalsupply += _value;       
         balance[_addr] += _value;     
     }
-
-    function burn(address _addr, uint _value) public returns  (string memory ) {
+function burn(address _addr, uint _value) public returns  (string memory ) {
         if (balance[_addr] >= _value) {
             balance[_addr] -= _value;     
             totalsupply -= _value; 
             }
         else {
-            return ("Not Enough Balance");
+            return ("There is not enough balance");
         }}
         }
